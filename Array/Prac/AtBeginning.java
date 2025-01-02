a48c683e162a38c65b671fd8bb338f6a424953e9
mrabhi198 <abhinavalok318@gmail.com>
2025-01-02 20:37:06 +0530

    ..

---
+++ Array/Prac/AtBeginning.java
@@ -0,0 +1,21 @@
+package Array.Prac;
+import java.util.*;
+
+public class AtBeginning {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int[] arr1 = new int[arr.length + 1];
+
+        for (int i = 0; i < arr1.length; i++){
+            
+        }
+    }
+}
---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,17 @@
+package Sorting;
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        int[] arr1 = {1, 2, 4, 23, 12, 5};
+
+       
+
+
+    }
+}
