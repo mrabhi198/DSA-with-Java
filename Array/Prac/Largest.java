8bab4dfdbc08c1da6733725228f0dc7439925820
mrabhi198 <abhinavalok318@gmail.com>
2025-01-02 22:36:03 +0530

    largest num

---
+++ Array/Prac/Largest.java
@@ -0,0 +1,25 @@
+package Array.Prac;
+import java.util.*;
+
+public interface Largest {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                
+            } 
+        }
+        System.out.println(max);
+        
+    }
+}
